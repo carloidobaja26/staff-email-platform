51155318d2b3c69f35d063c7407ead7f8c24e4b8
Microsoft <carlo.baja@c9.com.au>
2026-09-25 19:36:36 +0800

    migration script

---
+++ EmailPlatformInfrastructure/Persistence/Migrations/20260925110535_AddEmailJobRetryFields.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailPlatformInfrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailJobRetryFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AttemptCount",
+                table: "email_jobs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxAttempts",
+                table: "email_jobs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "NextAttemptAt",
+                table: "email_jobs",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttemptCount",
+                table: "email_jobs");
+
+            migrationBuilder.DropColumn(
+                name: "MaxAttempts",
+                table: "email_jobs");
+
+            migrationBuilder.DropColumn(
+                name: "NextAttemptAt",
+                table: "email_jobs");
+        }
+    }
+}
